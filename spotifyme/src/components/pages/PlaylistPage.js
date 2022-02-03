f6eaa774a507311ebbcf59a027920e599ed1a519
cdennelly99 <cdennelly@comcast.net>
2022-02-03 11:55:20 -0800

    started adding playlist grid. Still needs to be styled

--- spotifyme/src/components/pages/PlaylistPage.js
+++ spotifyme/src/components/pages/PlaylistPage.js
@@ -1,10 +1,53 @@
 import React from "react";
 import GeneralPage from "./GeneralPage";
+import { DataGrid } from "@mui/x-data-grid";
+
+
+
+const columns = [
+	{
+	  field: 'songTitle',
+	  headerName: 'Title',
+	  width: 400,
+	  editable: true,
+	},
+	{
+	  field: 'artist',
+	  headerName: 'Artist',
+	  width: 150,
+	  editable: true,
+	},
+	{
+	  field: 'Album',
+	  headerName: 'Album',
+	  width: 150,
+	  editable: true,
+	},
+	{
+	  field: 'time',
+	  headerName: 'Time',
+	  width: 100,
+	},
+  ];
+  
+  const rows = [
+	
+  ];
 
 export default function PlaylistPage() {
 	return (
 		<GeneralPage link="/homepage">
 			<h1>Playlist Page</h1>
+
+			<div style={{ height: 400, width: '100%' }}>
+      			<DataGrid
+        			rows={rows}
+        			columns={columns}
+        			pageSize={9}
+        			rowsPerPageOptions={[5]}
+        			
+      			/>
+    </div>
 		</GeneralPage>
 	);
 }
