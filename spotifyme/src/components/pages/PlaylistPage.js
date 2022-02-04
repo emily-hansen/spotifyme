245dec571a6f9699421fc05cc1834be0cfb564ae
cdennelly99 <cdennelly@comcast.net>
2022-02-04 11:24:49 -0800

    Playlist Page front end

--- spotifyme/src/components/pages/PlaylistPage.js
+++ spotifyme/src/components/pages/PlaylistPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import GeneralPage from "./GeneralPage";
 import { DataGrid } from "@mui/x-data-grid";
+import ICard from "../ItemCard";
+import { Stack} from "@mui/material";
+import { ColorButton } from "../Button";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -35,19 +39,55 @@ const columns = [
   ];
 
 export default function PlaylistPage() {
+	const navigator = useNavigate();
 	return (
 		<GeneralPage link="/homepage">
-			<h1>Playlist Page</h1>
+			
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+					height: "calc(100vh - 50px)",
+					width: "100vw",
+				}}>
+				<Stack
+					direction="column"
+					spacing={2}
+					style={{
+						alignItems: "center",
+					}}>
+					<ICard
+						nohome
+						head="Playlist"
+						style={{
+							width: "100%",
+							height: "100%",
+							justifyContent: "center",
+						}}>
+						<Stack direction="column" spacing={2} sx={{ alignItems: "center" }}>
+							<div style={{ height: 400, width: 1000, backgroundColor: "#181818" }}>
+      							<DataGrid
+        							rows={rows}
+        							columns={columns}
+        							pageSize={9}
+        							rowsPerPageOptions={[5]}
+									sx={{color: "white"}}
+      							/>
+    						</div>	
+						</Stack>
+					</ICard>
+					<ColorButton style={{ width: "150px", textTransform: "capitalize" }}
+						onClick={() => navigator("/homepage")}>
+						Create New
+					</ColorButton>
+				</Stack>
+			</div>
 
-			<div style={{ height: 400, width: '100%' }}>
-      			<DataGrid
-        			rows={rows}
-        			columns={columns}
-        			pageSize={9}
-        			rowsPerPageOptions={[5]}
-        			
-      			/>
-    </div>
+
+
+			
 		</GeneralPage>
 	);
 }
