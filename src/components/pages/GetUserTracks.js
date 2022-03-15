c9a82b547e25ced0809160d9b55dcf1d355cd663
ehansen <ehansen@ripple.com>
2022-03-15 14:46:02 -0700

    Cleanup pt3

--- src/components/pages/GetUserTracks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Cookies from "js-cookie";
-import { SpotifyApiContext } from "react-spotify-api";
-import { SpotifyAuthListener } from "react-spotify-auth";
-
-// Placeholder page; am thinking of migrating spotify call and parsing functionality to here
-const GetTracks = (props) => {};
-
-export default GetTracks;
